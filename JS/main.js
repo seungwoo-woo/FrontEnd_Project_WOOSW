556214ab2bb50faf33716a29c96d301de847b747
seungwoo-woo <seungwoo.woo@gmail.com>
2022-12-22 17:13:22 +0900

    information & about co. 추가

--- JS/main.js
+++ JS/main.js
@@ -25,3 +25,84 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()); // 컨트롤러에 장면을 할당
 });
+
+
+
+let wasCountDown = 0;
+window.addEventListener('scroll', function () {
+  
+  if (window.scrollY > 1400 && wasCountDown== 0) {
+    countUp();
+    wasCountDown = 1;
+  }
+
+  if (window.scrollY < 700 && wasCountDown == 1) {
+    wasCountDown = 0
+  }
+
+  function countUp() { 
+    let count1 = 1900;
+    let count2 = 0;
+    let count3 = 0;
+    let count4 = 0;
+    let count5 = 0;
+    const countEl1 = document.querySelector('.count-no1');
+    const countEl2 = document.querySelector('.count-no2');
+    const countEl3 = document.querySelector('.count-no3');
+    const countEl4 = document.querySelector('.count-no4');
+    const countEl5 = document.querySelector('.count-no5');  
+
+    let id1 = setInterval(function(){no1_change()},1000/122);
+    let id2 = setInterval(function(){no2_change()},1000/99);
+    let id3 = setInterval(function(){no3_change()},1000/99);
+    let id4 = setInterval(function(){no4_change()},1000/26);
+    let id5 = setInterval(function(){no5_change()},1000/21);
+
+    function no1_change() {
+      count1 = count1 + 1;
+      if(count1 > 2022) {
+        clearInterval(id1);
+      } else {
+        countEl1.innerHTML = `${count1}년`;
+      }      
+    }
+
+    function no2_change() {
+      count2 = count2 + 1;
+      if(count2 > 99) {
+        clearInterval(id2);
+      } else {
+        countEl2.innerHTML = `${count2}억`;
+      }      
+    }
+
+    function no3_change() {
+      count3 = count3 + 1;
+      if(count3 > 99) {
+        clearInterval(id3);
+      } else {
+        countEl3.innerHTML = `${count3}개`;
+      }      
+    }
+
+    function no4_change() {
+      count4 = count4 + 1;
+      if(count4 > 26) {
+        clearInterval(id4);
+      } else {
+        countEl4.innerHTML = `${count4}개`;
+      }      
+    }
+
+    function no5_change() {
+      count5 = count5 + 1;
+      if(count5 > 21) {
+        clearInterval(id5);
+      } else {
+        countEl5.innerHTML = `${count5}명`;
+      }      
+    }
+
+  }
+
+})
